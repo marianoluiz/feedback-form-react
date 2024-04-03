5728668c88738153bccc09681aa287420eafca4a
Richa Arora <richaaeri9988@gmail.com>
2024-04-03 15:56:11 +0530

    feedback

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -8,7 +8,7 @@ const FeedbackForm = () => {
     <nav>
     Tell Us What You Think
     </nav>
-      <form onSubmit={handleSubmit} className="feedback-form">
+      <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         
