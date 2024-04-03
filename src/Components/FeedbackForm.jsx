9823be86f49cff0b67bc72f10d2888f56970a9a7
Richa Arora <richaaeri9988@gmail.com>
2024-04-03 14:35:07 +0530

    initial commit

---
+++ src/Components/FeedbackForm.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './FeedbackForm.css'; // Import CSS for styling
+
+const FeedbackForm = () => {
+
+  return (
+    <>
+    <nav>
+    Tell Us What You Think
+    </nav>
+      <form onSubmit={handleSubmit} className="feedback-form">
+        <h2>We'd Love to Hear From You!</h2>
+        <p>Please share your feedback with us.</p>
+        
+      </form>
+    </>
+  );
+};
+
+export default FeedbackForm;
